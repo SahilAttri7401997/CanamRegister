3c4b0b1634ec189b3fb345ac23bb0b61a63cf534
SahilAttri740 <Sahil.attri@techrbm.com>
2024-10-13 00:41:23 +0530

    push to origin

--- obj/Debug/net8.0/CanamRegister.AssemblyInfo.cs
+++ obj/Debug/net8.0/CanamRegister.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CanamRegister")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc195e0f92df1291329244c99d666622aaf701d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3afd0b6274c4fa452905b51a5fb9c7dbd5877c67")]
 [assembly: System.Reflection.AssemblyProductAttribute("CanamRegister")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CanamRegister")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
